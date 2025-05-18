3fa8eaf2b1e80c80b0789eeef9c15412f69b666b
DANS <Dansnyagechi@gmail.com>
2025-05-18 11:16:40 +0300

    app id changed

--- src/components/shared/utils/config/config.ts
+++ src/components/shared/utils/config/config.ts
@@ -7,7 +7,7 @@ export const APP_IDS = {
     STAGING: 29934,
     STAGING_BE: 29934,
     STAGING_ME: 29934,
-    PRODUCTION: 71916,
+    PRODUCTION: 76128,
     PRODUCTION_BE: 65556,
     PRODUCTION_ME: 65557,
 };
@@ -63,7 +63,7 @@ export const getDefaultAppIdAndUrl = () => {
 
 export const getAppId = () => {
     const stored_app_id = window.localStorage.getItem('config.app_id');
-    if (stored_app_id && stored_app_id !== '71916') {
+    if (stored_app_id && stored_app_id !== '76128') {
         window.localStorage.removeItem('config.app_id');
     }
 
@@ -156,7 +156,7 @@ export const generateOAuthURL = () => {
     const oauth_url = getOauthURL();
     const original_url = new URL(oauth_url);
 
-    const app_id = '71916';
+    const app_id = '76128';
     localStorage.setItem('config.app_id', app_id);
 
     original_url.hostname = 'ws.derivws.com';
@@ -169,7 +169,7 @@ export const generateOAuthURL = () => {
 if (typeof window !== 'undefined') {
     if (isLocal() || isTestLink()) {
         localStorage.removeItem('config.server_url');
-        localStorage.setItem('config.app_id', '71916');
+        localStorage.setItem('config.app_id', '76128');
     }
 
     checkAndSetEndpointFromUrl();
